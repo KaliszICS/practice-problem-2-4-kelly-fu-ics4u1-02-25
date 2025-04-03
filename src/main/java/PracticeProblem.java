34a7eec5a5be822807259fd7a5ff439173d867b3
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-04-03 01:23:29 +0000

    why the system doesn't tell me what's wrong with my code???

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,6 +3,45 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static void selectionSortName(String[] names, int[] ages){
+		for (int i = 0; i<names.length; i++){
+			names[i] = names[i].toLowerCase();
+		}
+		for (int i = 0; i<names.length -1; i++){
+			//find the min
+			int indexOfMin = i;
+			for (int j = i + 1; j <names.length; j++){
+				if (names[j].compareTo(names[indexOfMin])<0){
+					indexOfMin = j;
+				}
+			}
+			String tempName = names[i];
+			names[i] = names[indexOfMin];
+			names[indexOfMin] = tempName;
+			int tempAges = ages[i];
+			ages[i] = ages[indexOfMin];
+			ages[indexOfMin] = tempAges;
+		}
+	}
+
+
+	public static void selectionSortAge(String[] names, int[] ages){
+		for (int i = 0; i<names.length -1; i++){
+			//find the min
+			int indexOfMin = i;
+			for (int j = i + 1; j <names.length; j++){
+				if (names[j].compareTo(names[indexOfMin])<0){
+					indexOfMin = j;
+				}
+			}
+			String tempName = names[i];
+			names[i] = names[indexOfMin];
+			names[indexOfMin] = tempName;
+			int tempAges = ages[i];
+			ages[i] = ages[indexOfMin];
+			ages[indexOfMin] = tempAges;
+		}
+	}
 
 	
 
